fdd5ae89711055c8bfa3ff12b8436090a8c07294
Veronika Valentinova <vgvalentinova@abv.bg>
2019-12-29 10:40:28 +0200

    Created spring project

---
+++ backend/src/main/java/bg/sofia/uni/fmi/tbb/TbbApplication.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.tbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbbApplication.class, args);
+	}
+
+}
