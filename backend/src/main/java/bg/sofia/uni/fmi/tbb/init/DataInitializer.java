ec3896adc9b73825c6084bcf21ffccdc2696aaf6
Veronika Valentinova <vgvalentinova@abv.bg>
2020-01-31 11:17:28 +0200

    Removed unused role

--- backend/src/main/java/bg/sofia/uni/fmi/tbb/init/DataInitializer.java
+++ backend/src/main/java/bg/sofia/uni/fmi/tbb/init/DataInitializer.java
@@ -1,15 +1,12 @@
 package bg.sofia.uni.fmi.tbb.init;
 
 import bg.sofia.uni.fmi.tbb.domain.UsersService;
-import bg.sofia.uni.fmi.tbb.model.Role;
 import bg.sofia.uni.fmi.tbb.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 
 @Slf4j
