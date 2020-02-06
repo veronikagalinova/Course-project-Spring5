bd0df8c51abd5f99ce32f73d47a28fec467186b0
Veronika Valentinova <vgvalentinova@abv.bg>
2020-02-06 20:06:52 +0200

    Modified time field

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CompanyLinesComponent } from './components/bus-company/company-lines/company-lines.component';
@@ -21,6 +22,18 @@ import { AuthenticationService } from './_services/authentication.service';
 import { SearchRouteComponent } from './components/traveler/search-route/search-route.component';
 import { RouterModule } from '@angular/router';
 import { CreateBusLineComponent } from './components/bus-company/create-bus-line/create-bus-line.component';
+import { OverlayContainer } from '@angular/cdk/overlay';
+
+export class CdkOverlayContainer extends OverlayContainer {
+
+  protected _createContainer(): void {
+    const container = this._document.createElement('div');
+
+    container.classList.add('cdk-overlay-container', 'test');
+    this._document.body.appendChild(container);
+    this._containerElement = container;
+  }
+}
 
 @NgModule({
   declarations: [
@@ -44,11 +57,13 @@ import { CreateBusLineComponent } from './components/bus-company/create-bus-line
     FormsModule,
     ReactiveFormsModule,
     RouterModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxMaterialTimepickerModule
   ],
   providers: [
     AuthenticationService, AuthGuard,
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: OverlayContainer, useClass: CdkOverlayContainer }
   ],
   bootstrap: [AppComponent],
   entryComponents: [
